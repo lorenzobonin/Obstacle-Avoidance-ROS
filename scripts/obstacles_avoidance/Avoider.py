c89293d67342e7c5fa71ee4635807792fc7ee2a3
Leonardo Arrighi <s232661@ds.units.it>
2024-02-20 12:12:06 +0100

    Added all comments to avoider

--- scripts/obstacles_avoidance/Avoider.py
+++ scripts/obstacles_avoidance/Avoider.py
@@ -3,14 +3,15 @@
 class Avoider():
 	''' This class provides simple obstacle avoidance functionalities to a ROS robot '''
 
-	# This dict keeps track of the distance measures for each region
+	# Costruiamo un dizionario per tenere traccia delle distanze delle varie regioni 
 	Regions_Report = {
 	                     "front_C": [], "front_L": [], "left_R" : [],
 	                     "left_C" : [], "left_L" : [], "back_R" : [],
 	                     "back_C" : [], "back_L" : [], "right_R": [],
 	                     "right_C": [], "right_L": [], "front_R": [],
 	                 }
-	# These are the costs to deviate from each region to the goal region (front_C)
+	
+	# Associamo un costo per ruotare ad ogni regione (front_C)
 	Regions_Distances = {
 	                     "front_C":  0, "front_L":  1, "left_R" :  2,
 	                     "left_C" :  3, "left_L" :  4, "back_R" :  5,
@@ -18,9 +19,14 @@ class Avoider():
 	                     "right_C": -3, "right_L": -2, "front_R": -1,
 	                 	}
 
-	def __init__(self, vel_obj, obstacle_threshold=0.5, 
-				       regional_angle=30, normal_lin_vel=0.1, 
-				       trans_lin_vel=-0.09, trans_ang_vel=0.5):
+	# Costruttore: che cosa ci serve?
+	def __init__(self, 
+			  	vel_obj, 
+			  	obstacle_threshold=0.5, 
+				regional_angle=30, 
+				normal_lin_vel=0.1, 
+				trans_lin_vel=-0.09, 
+				trans_ang_vel=0.5):
 		'''
 		:param vel_obj           : Velocity object; will contain velocity commands(data); Twist()
 		:param obstacle_threshold: Objects a this distance or below are considered obstacles
@@ -37,12 +43,13 @@ class Avoider():
 		self.TRANS_LIN_VEL  = trans_lin_vel
 		self.TRANS_ANG_VEL  = trans_ang_vel
 
+	# Funzione che identifica le regioni attorno al robot
 	def indentify_regions(self, scan):
 		'''
-		:param scan: Scan object that contains the lidar data 
+		:param scan: Scannerizza gli oggetti contenuti nei dati del LIDAR 
 		'''
 
-		# This list keeps track of the order in which the regions' readings are obtained
+		# Lista per tracciare le regioni
 		REGIONS = [
 		             "front_C", "front_L", "left_R" ,
 		             "left_C" , "left_L" , "back_R" ,
@@ -50,47 +57,85 @@ class Avoider():
 		             "right_C", "right_L", "front_R",
 				  ]
 
-		# The front central region necessitate getting the last and first 15 points of the ranges
+		# Scannerizziamo la prima regione: frontale che va da 15 a -15 gradi rispetto al centro dello scanner
 		intermediary = scan.ranges[:int(self.REGIONAL_ANGLE/2)]\
 					 + scan.ranges[(len(scan.ranges)-1)*int(self.REGIONAL_ANGLE/2):]
-		self.Regions_Report["front_C"] = [x for x in intermediary if x < self.OBSTACLE_DIST and x != 'inf' and x !=0]
 		
-		# Enumerate all regions but the first
+		# Controlliamo la regione frontale è libera, in caso positivo salviamo il risultato della scannerizzazione
+		new_front_scan = []
+		for x in intermediary: 
+			if x < self.OBSTACLE_DIST and x!= 'inf' and x!= 0:
+				new_front_scan.append(x)
+		self.Regions_Report["front_C"] = new_front_scan
+
+		# Numeriamo tutte le altre regioni...
 		for i, region in enumerate(REGIONS[1:]):
-			# Only objects at a distance less than or equal to the threshold are considered obstacles
-			self.Regions_Report[region] = [x for x in scan.ranges[self.REGIONAL_ANGLE*i:self.REGIONAL_ANGLE*(i+1)]\
-												   if x < self.OBSTACLE_DIST and x != 'inf' and x !=0]
+			
+			# ...e scannerizziamole come fatto per la centrale
+			new_scan = []
+			for x in scan.ranges[self.REGIONAL_ANGLE*i : self.REGIONAL_ANGLE*(i+1)]:
+				
+				# Salviamo il risultato se sono libere
+				if (x < self.OBSTACLE_DIST and x != 'inf' and x !=0):
+					new_scan.append(x)
+			
+			self.Regions_Report[region] = new_scan
+			
 
-	def avoid(self):
-		act, ang_vel = self._clearance_test()
-		# If act is False, ang_vel is set to 0
-		self._steer(act, (act*ang_vel))
-		return self.vel_obj
 
+	# Funzione che calcola il clearest path
 	def _clearance_test(self):
+
+		# Impostiamo i parametri desiderati
 		goal = "front_C"
 		closest = 10e6
 		regional_dist = 0
 		maxima = {"destination": "back_C", "distance": 10e-6}
+
+		# Scannerizziamo le regioni
 		for region in self.Regions_Report.items():
+
+			# Valutiamo le distanze delle varie regioni (ricordiamo che le regioni vengono salvate in un ordine preciso)
 			regional_dist = abs(self.Regions_Distances[region[0]]-self.Regions_Distances[goal])
-			#if there're no obstacles in that region
-			if not len(region[1]):
-				#check if it's the cheapest option
+			
+			# Se non ci sono ostacoli nella regione trovata...
+			if len(region[1]) == 0:
+
+				# ... controlliamo che sia la regione meno costosa
 				if (regional_dist < closest):
+
+					# Sovrascriviamo i parametri in caso positivo
 					closest = regional_dist
 					maxima["distance"] = self.OBSTACLE_DIST
 					maxima["destination"] = region[0]
-			#check if it's the clearest option
+			
+			# Altrimenti salviamola lo stesso: potrebbe essere comunque il path più libero
 			elif(max(region[1]) > maxima["distance"]):
+
 				maxima["distance"] = max(region[1])
 				maxima["destination"] = region[0]
-		#calculate the cost to the chosen orientation
-		regional_dist = self.Regions_Distances[maxima["destination"]]-self.Regions_Distances[goal]
-		
-		# Return whether to act or not, and the angular velocity with the appropriate sign
-		return (closest != 0), (regional_dist/[abs(regional_dist) if regional_dist != 0 else 1][0])*self.TRANS_ANG_VEL
 
+		# Calcoliamo il costo di rotazione del robot
+		regional_dist = self.Regions_Distances[maxima["destination"]] - self.Regions_Distances[goal]
+		
+		# Risultati: ci muoviamo? (torniamo "act" come vero o falso)
+		if closest != 0:
+			act = True
+		else:
+			act = False
+		
+		# Risultati: ruotiamo? (torniamo "ang_vel" con il segno appropriato)
+		if regional_dist == 0:
+			denom = 1
+		else:
+			denom = abs(regional_dist)
+		
+		ang_vel = (regional_dist / denom) * self.TRANS_ANG_VEL
+		
+		return act, ang_vel
+	
+
+	# Funzione per guidare il robot
 	def _steer(self, steer=False, ang_vel=0):
 		'''
 		:param steer  : Whether to avoid and obstacle or keep on going straigt
@@ -105,3 +150,18 @@ class Avoider():
 		self.vel_obj.angular.x = 0
 		self.vel_obj.angular.y = 0
 		self.vel_obj.angular.z = ang_vel
+
+
+	# Funzione avoid: controlla la velocità del robot		
+	def avoid(self):
+
+		# Controlliamo se il percorso è libero
+		act, ang_vel = self._clearance_test()
+
+		# Se "act" è False, "ang_vel" è 0
+		if act == False:
+			ang_vel = 0.0
+
+		self._steer(act, ang_vel)
+
+		return self.vel_obj
